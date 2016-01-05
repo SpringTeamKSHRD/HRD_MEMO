6b9c1a08dc47085e6b07c06e90e41e2ed821b9c2
Chhorn Elit <chhornelit@gmail.com>
2016-01-05 17:33:22 +0700

    update db script

---
+++ dbscript/memo.sql
@@ -0,0 +1,258 @@
+/*
+Navicat PGSQL Data Transfer
+
+Source Server         : postgres
+Source Server Version : 90303
+Source Host           : localhost:5432
+Source Database       : memotest1
+Source Schema         : memo
+
+Target Server Type    : PGSQL
+Target Server Version : 90303
+File Encoding         : 65001
+
+Date: 2016-01-05 17:14:33
+*/
+
+CREATE SCHEMA memo;
+-- ----------------------------
+-- Sequence structure for tbcategory_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "memo"."tbcategory_id_seq" CASCADE;
+CREATE SEQUENCE "memo"."tbcategory_id_seq"
+ INCREMENT 1
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 1
+ CACHE 1;
+SELECT setval('"memo"."tbcategory_id_seq"', 1, true);
+
+-- ----------------------------
+-- Sequence structure for tbhistory_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "memo"."tbhistory_id_seq" CASCADE;
+CREATE SEQUENCE "memo"."tbhistory_id_seq"
+ INCREMENT 1
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 2
+ CACHE 1;
+SELECT setval('"memo"."tbhistory_id_seq"', 2, true);
+
+-- ----------------------------
+-- Sequence structure for tbmemo_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "memo"."tbmemo_id_seq" CASCADE;
+CREATE SEQUENCE "memo"."tbmemo_id_seq"
+ INCREMENT 1
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 1
+ CACHE 1;
+SELECT setval('"memo"."tbmemo_id_seq"', 1, true);
+
+-- ----------------------------
+-- Sequence structure for tbmessage_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "memo"."tbmessage_id_seq" CASCADE;
+CREATE SEQUENCE "memo"."tbmessage_id_seq"
+ INCREMENT 1
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 1
+ CACHE 1;
+SELECT setval('"memo"."tbmessage_id_seq"', 1, true);
+
+-- ----------------------------
+-- Sequence structure for tbreport_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "memo"."tbreport_id_seq" CASCADE;
+CREATE SEQUENCE "memo"."tbreport_id_seq"
+ INCREMENT 1
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 1
+ CACHE 1;
+SELECT setval('"memo"."tbreport_id_seq"', 1, true);
+
+-- ----------------------------
+-- Table structure for tbcategory
+-- ----------------------------
+DROP TABLE IF EXISTS "memo"."tbcategory" CASCADE;
+CREATE TABLE "memo"."tbcategory" (
+"id" int4 DEFAULT nextval('"memo".tbcategory_id_seq'::regclass) NOT NULL,
+"category" varchar COLLATE "default" NOT NULL,
+"description" varchar COLLATE "default"
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tbcategory
+-- ----------------------------
+INSERT INTO "memo"."tbcategory" VALUES ('1', 'sport', 'sadfasdf');
+
+-- ----------------------------
+-- Table structure for tbhistory
+-- ----------------------------
+DROP TABLE IF EXISTS "memo"."tbhistory" CASCADE;
+CREATE TABLE "memo"."tbhistory" (
+"id" int8 DEFAULT nextval('"memo".tbhistory_id_seq'::regclass) NOT NULL,
+"memoid" int4 NOT NULL,
+"title" varchar COLLATE "default" NOT NULL,
+"content" varchar COLLATE "default",
+"date" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tbhistory
+-- ----------------------------
+INSERT INTO "memo"."tbhistory" VALUES ('2', '1', 'sdf', 'sdf', '2016-01-05 17:10:23.587');
+
+-- ----------------------------
+-- Table structure for tbmemo
+-- ----------------------------
+DROP TABLE IF EXISTS "memo"."tbmemo" CASCADE;
+CREATE TABLE "memo"."tbmemo" (
+"id" int4 DEFAULT nextval('"memo".tbmemo_id_seq'::regclass) NOT NULL,
+"userid" int4 NOT NULL,
+"titlememo" varchar COLLATE "default" NOT NULL,
+"content" varchar COLLATE "default",
+"titleurl" varchar COLLATE "default",
+"domain" varchar COLLATE "default",
+"url" varchar(32) COLLATE "default" NOT NULL,
+"date" timestamp(6) NOT NULL,
+"categoryid" int4 NOT NULL,
+"isenable" bool NOT NULL,
+"ispublic" bool NOT NULL
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tbmemo
+-- ----------------------------
+INSERT INTO "memo"."tbmemo" VALUES ('1', '1', 'sdfsdfsdfs', 'sdf', 'sdf', 'sdf', 'sdf', '2016-01-05 17:09:58', '1', 't', 't');
+
+-- ----------------------------
+-- Table structure for tbmessage
+-- ----------------------------
+DROP TABLE IF EXISTS "memo"."tbmessage" CASCADE;
+CREATE TABLE "memo"."tbmessage" (
+"id" int4 DEFAULT nextval('"memo".tbmessage_id_seq'::regclass) NOT NULL,
+"userid" int4 NOT NULL,
+"isviewed" bool DEFAULT false NOT NULL
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tbmessage
+-- ----------------------------
+INSERT INTO "memo"."tbmessage" VALUES ('1', '1', 'f');
+
+-- ----------------------------
+-- Table structure for tbreport
+-- ----------------------------
+DROP TABLE IF EXISTS "memo"."tbreport" CASCADE;
+CREATE TABLE "memo"."tbreport" (
+"id" int4 DEFAULT nextval('"memo".tbreport_id_seq'::regclass) NOT NULL,
+"reporterid" int4 NOT NULL,
+"memoid" int4 NOT NULL,
+"description" varchar COLLATE "default",
+"date" timestamp(6) NOT NULL,
+"isblocked" bool DEFAULT false NOT NULL
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tbreport
+-- ----------------------------
+INSERT INTO "memo"."tbreport" VALUES ('1', '1', '1', 'asdasdf', '2016-01-05 17:10:58', 'f');
+
+-- ----------------------------
+-- Alter Sequences Owned By 
+-- ----------------------------
+ALTER SEQUENCE "memo"."tbcategory_id_seq" OWNED BY "memo"."tbcategory"."id";
+ALTER SEQUENCE "memo"."tbhistory_id_seq" OWNED BY "memo"."tbhistory"."id";
+ALTER SEQUENCE "memo"."tbmemo_id_seq" OWNED BY "memo"."tbmemo"."id";
+ALTER SEQUENCE "memo"."tbmessage_id_seq" OWNED BY "memo"."tbmessage"."id";
+ALTER SEQUENCE "memo"."tbreport_id_seq" OWNED BY "memo"."tbreport"."id";
+
+-- ----------------------------
+-- Primary Key structure for table tbcategory
+-- ----------------------------
+ALTER TABLE "memo"."tbcategory" ADD PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table tbhistory
+-- ----------------------------
+ALTER TABLE "memo"."tbhistory" ADD PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Function Trigger insert_tbhistory
+-- ----------------------------
+CREATE or replace FUNCTION "memo"."insert_tbhistory"()
+  RETURNS trigger AS $BODY$
+BEGIN
+ IF (TG_OP = 'UPDATE') THEN
+
+INSERT INTO "memo"."tbhistory" ("memoid", "title", "content", "date") VALUES(OLD.id,OLD.titlememo,OLD.content,now());
+    return null;
+        END IF;
+END
+$BODY$
+  LANGUAGE 'plpgsql' VOLATILE COST 100
+;
+ALTER FUNCTION "memo"."insert_tbhistory"() OWNER TO "postgres";
+-- ----------------------------
+-- Triggers structure for table tbmemo
+-- ----------------------------
+CREATE TRIGGER "triggger_insert_tbhistory" AFTER UPDATE ON "memo"."tbmemo"
+FOR EACH ROW
+EXECUTE PROCEDURE "memo"."insert_tbhistory"();
+
+-- ----------------------------
+-- Primary Key structure for table tbmemo
+-- ----------------------------
+ALTER TABLE "memo"."tbmemo" ADD PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table tbmessage
+-- ----------------------------
+ALTER TABLE "memo"."tbmessage" ADD PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table tbreport
+-- ----------------------------
+ALTER TABLE "memo"."tbreport" ADD PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Foreign Key structure for table "memo"."tbhistory"
+-- ----------------------------
+ALTER TABLE "memo"."tbhistory" ADD FOREIGN KEY ("memoid") REFERENCES "memo"."tbmemo" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Key structure for table "memo"."tbmemo"
+-- ----------------------------
+ALTER TABLE "memo"."tbmemo" ADD FOREIGN KEY ("categoryid") REFERENCES "memo"."tbcategory" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "memo"."tbmemo" ADD FOREIGN KEY ("userid") REFERENCES "public"."tbluser" ("userid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Key structure for table "memo"."tbmessage"
+-- ----------------------------
+ALTER TABLE "memo"."tbmessage" ADD FOREIGN KEY ("userid") REFERENCES "public"."tbluser" ("userid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Key structure for table "memo"."tbreport"
+-- ----------------------------
+ALTER TABLE "memo"."tbreport" ADD FOREIGN KEY ("reporterid") REFERENCES "public"."tbluser" ("userid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "memo"."tbreport" ADD FOREIGN KEY ("memoid") REFERENCES "memo"."tbmemo" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
---
+++ dbscript/public.sql
@@ -0,0 +1,112 @@
+/*
+Navicat PGSQL Data Transfer
+
+Source Server         : postgres
+Source Server Version : 90303
+Source Host           : localhost:5432
+Source Database       : memotest1
+Source Schema         : public
+
+Target Server Type    : PGSQL
+Target Server Version : 90303
+File Encoding         : 65001
+
+Date: 2016-01-05 17:14:42
+*/
+
+
+-- ----------------------------
+-- Sequence structure for tbluser_userid_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."tbluser_userid_seq" CASCADE;
+CREATE SEQUENCE "public"."tbluser_userid_seq"
+ INCREMENT 1
+ MINVALUE 1
+ MAXVALUE 9223372036854775807
+ START 2
+ CACHE 1;
+SELECT setval('"public"."tbluser_userid_seq"', 2, true);
+
+-- ----------------------------
+-- Table structure for tbluser
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."tbluser" CASCADE;
+CREATE TABLE "public"."tbluser" (
+"userid" int4 DEFAULT nextval('tbluser_userid_seq'::regclass) NOT NULL,
+"email" text COLLATE "default" NOT NULL,
+"password" text COLLATE "default" NOT NULL,
+"username" text COLLATE "default",
+"gender" text COLLATE "default",
+"dateofbirth" date,
+"phonenumber" text COLLATE "default",
+"registerdate" timestamp(6),
+"userimageurl" text COLLATE "default",
+"usertypeid" int4,
+"point" int2,
+"universityid" int4,
+"departmentid" int4,
+"userstatus" bit(1),
+"ismemoenabled" bool DEFAULT true NOT NULL
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tbluser
+-- ----------------------------
+INSERT INTO "public"."tbluser" VALUES ('1', 'admin@gmail.com', '1', 'admin', 'male', '2013-04-18', '12345678', '2015-01-13 16:45:23', 'avatar.jpg', '5', null, '36', '12', E'1', 't');
+INSERT INTO "public"."tbluser" VALUES ('2', 'user@gmail.com', '1', 'user', 'male', '2016-01-05', '1234123412', '2016-01-05 17:12:08', 'avatar.jpg', '2', null, '36', '12', E'1', 't');
+
+-- ----------------------------
+-- Table structure for tblusertype
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."tblusertype";
+CREATE TABLE "public"."tblusertype" (
+"usertypeid" int4 NOT NULL,
+"usertypename" text COLLATE "default",
+"viewable" bool,
+"commentable" bool,
+"postable" bool,
+"deleteable" bool,
+"userable" bool
+)
+WITH (OIDS=FALSE)
+
+;
+
+-- ----------------------------
+-- Records of tblusertype
+-- ----------------------------
+INSERT INTO "public"."tblusertype" VALUES ('1', 'Visitor', 'f', 'f', 'f', 'f', 'f');
+INSERT INTO "public"."tblusertype" VALUES ('2', 'Subscriber', 't', 't', 'f', 'f', 'f');
+INSERT INTO "public"."tblusertype" VALUES ('3', 'Editor', 't', 't', 't', 'f', 'f');
+INSERT INTO "public"."tblusertype" VALUES ('4', 'Moderator', 't', 't', 't', 't', 'f');
+INSERT INTO "public"."tblusertype" VALUES ('5', 'Admin', 't', 't', 't', 't', 't');
+INSERT INTO "public"."tblusertype" VALUES ('6', 'Employee', 't', 't', 't', 't', 't');
+INSERT INTO "public"."tblusertype" VALUES ('7', 'Company', 't', 't', 't', 't', 't');
+
+-- ----------------------------
+-- Alter Sequences Owned By 
+-- ----------------------------
+ALTER SEQUENCE "public"."tbluser_userid_seq" OWNED BY "tbluser"."userid";
+
+-- ----------------------------
+-- Uniques structure for table tbluser
+-- ----------------------------
+ALTER TABLE "public"."tbluser" ADD UNIQUE ("email");
+
+-- ----------------------------
+-- Primary Key structure for table tbluser
+-- ----------------------------
+ALTER TABLE "public"."tbluser" ADD PRIMARY KEY ("userid");
+
+-- ----------------------------
+-- Primary Key structure for table tblusertype
+-- ----------------------------
+ALTER TABLE "public"."tblusertype" ADD PRIMARY KEY ("usertypeid");
+
+-- ----------------------------
+-- Foreign Key structure for table "public"."tbluser"
+-- ----------------------------
+ALTER TABLE "public"."tbluser" ADD FOREIGN KEY ("usertypeid") REFERENCES "public"."tblusertype" ("usertypeid") ON DELETE NO ACTION ON UPDATE NO ACTION;
