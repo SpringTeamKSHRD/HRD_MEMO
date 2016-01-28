4ea953718f8c9464ae4aa839715eb81aafa1a907
sona-ky <kysona93@gmail.com>
2016-01-28 11:39:53 +0700

    For present

--- src/main/webapp/resources/user/js/add_memo.js
+++ src/main/webapp/resources/user/js/add_memo.js
@@ -5,31 +5,36 @@ function saveMemo(){
 	var data=$("#cont_memo").val();
 	var ispublic;
 	
-	if($("#privacy").val()==0) ispublic=false;
-	else ispublic=true;
+	if($('.titlememo').val()=="" || data==""){
+		sweetAlert("Fail", "Title and content memo cannot be empty!", "error");
+	}else{
+		if($("#privacy").val()==0) ispublic=false;
+		else ispublic=true;
+		
+		json = {userid : parseInt($('#userid').val()),
+				title : $('.titlememo').val(),
+				content : data,
+				titleurl : "Memo Dashboard",
+				domain : "www.khmeracademy.org.kh",
+				url : "http://khmeracademy.org.kh",
+				isenable : true,
+				ispublic : ispublic
+			};
+		
+		$.ajax({
+			type : "POST",
+			url : domain + "/user/",
+			data : JSON.stringify(json),
+			contentType: 'application/json',
+			success : function(data) {
+				swal("Success","Your memo has been saved.","success");
+				loadPage();
+			},
+			error : function(data) {
+				sweetAlert("Fail", "Fail with create memo!", "error");
+				console.log("ERROR..." + data);
+			}
+		});
+	}
 	
-	json = {userid : parseInt($('#userid').val()),
-			title : $('.titlememo').val(),
-			content : data,
-			titleurl : "Memo Dashboard",
-			domain : "www.khmeracademy.org.kh",
-			url : "http://khmeracademy.org.kh",
-			isenable : true,
-			ispublic : ispublic
-		};
-	
-	$.ajax({
-		type : "POST",
-		url : domain + "/user/",
-		data : JSON.stringify(json),
-		contentType: 'application/json',
-		success : function(data) {
-			swal("Success","Your memo has been saved.","success");
-			loadPage();
-		},
-		error : function(data) {
-			sweetAlert("Fail", "Fail with create memo!", "error");
-			console.log("ERROR..." + data);
-		}
-	});
 }
